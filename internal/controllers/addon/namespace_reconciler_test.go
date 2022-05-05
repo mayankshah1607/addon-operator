7bdc2a1d0fbccdbf40e38c653195d150ad7072db
Mayank Shah <m.shah@redhat.com>
2022-05-05 12:55:16 +0530

    refactor: new sub-reconciler for namespace reconciliation
    Signed-off-by: Mayank Shah <m.shah@redhat.com>

--- internal/controllers/addon/namespace_reconciler_test.go
+++ internal/controllers/addon/namespace_reconciler_test.go
@@ -2,6 +2,7 @@ package addon
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,9 +22,8 @@ import (
 func TestEnsureWantedNamespaces_AddonWithoutNamespaces(t *testing.T) {
 	c := testutil.NewClient()
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -40,9 +40,8 @@ func TestEnsureWantedNamespaces_AddonWithSingleNamespace_Collision(t *testing.T)
 		arg := args.Get(2).(*corev1.Namespace)
 		testutil.NewTestExistingNamespace().DeepCopyInto(arg)
 	}).Return(nil)
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -72,9 +71,8 @@ func TestEnsureWantedNamespaces_AddonWithSingleNamespace_NoCollision(t *testing.
 		}
 	}).Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -97,9 +95,8 @@ func TestEnsureWantedNamespaces_AddonWithMultipleNamespaces_NoCollision(t *testi
 		}
 	}).Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -135,9 +132,8 @@ func TestEnsureWantedNamespaces_AddonWithMultipleNamespaces_SingleCollision(t *t
 		}).
 		Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -165,9 +161,8 @@ func TestEnsureWantedNamespaces_AddonWithMultipleNamespaces_MultipleCollisions(t
 		}).
 		Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -194,9 +189,8 @@ func TestEnsureNamespace_Create(t *testing.T) {
 	c.On("Get", testutil.IsContext, testutil.IsObjectKey, testutil.IsCoreV1NamespacePtr).Return(testutil.NewTestErrNotFound())
 	c.On("Create", testutil.IsContext, testutil.IsCoreV1NamespacePtr, mock.Anything).Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -220,9 +214,8 @@ func TestEnsureNamespace_CreateWithLabels(t *testing.T) {
 		}).
 		Return(nil)
 
-	r := &AddonReconciler{
+	r := &namespaceReconciler{
 		Client: c,
-		Log:    testutil.NewLogger(t),
 		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
 	}
 
@@ -375,4 +368,124 @@ func TestReconcileNamespace_CreateWithClientError(t *testing.T) {
 	c.AssertCalled(t, "Get", testutil.IsContext, client.ObjectKey{
 		Name: "namespace-1",
 	}, testutil.IsCoreV1NamespacePtr)
+}
+
+func TestEnsureDeletionOfUnwantedNamespaces_NoNamespacesInSpec_NoNamespacesInCluster(t *testing.T) {
+	c := testutil.NewClient()
+
+	c.On("List", testutil.IsContext, testutil.IsCoreV1NamespaceListPtr, mock.Anything).
+		Return(nil)
+
+	r := &namespaceReconciler{
+		Client: c,
+		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
+	}
+
+	ctx := context.Background()
+	err := r.ensureDeletionOfUnwantedNamespaces(ctx, testutil.NewTestAddonWithoutNamespace())
+	require.NoError(t, err)
+	c.AssertExpectations(t)
+}
+
+func TestEnsureDeletionOfUnwantedNamespaces_NoNamespacesInSpec_NamespaceInCluster(t *testing.T) {
+	c := testutil.NewClient()
+
+	addon := testutil.NewTestAddonWithoutNamespace()
+	existingNamespace := testutil.NewTestNamespace()
+
+	c.On("List", testutil.IsContext, testutil.IsCoreV1NamespaceListPtr, mock.Anything).
+		Run(func(args mock.Arguments) {
+			arg := args.Get(1).(*corev1.NamespaceList)
+			namespaceList := corev1.NamespaceList{
+				Items: []corev1.Namespace{
+					*existingNamespace,
+				},
+			}
+			namespaceList.DeepCopyInto(arg)
+		}).
+		Return(nil)
+	c.On("Delete", testutil.IsContext, testutil.IsCoreV1NamespacePtr, mock.Anything).
+		Return(nil)
+
+	r := &namespaceReconciler{
+		Client: c,
+		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
+	}
+
+	ctx := context.Background()
+	err := r.ensureDeletionOfUnwantedNamespaces(ctx, addon)
+	require.NoError(t, err)
+	c.AssertExpectations(t)
+	c.AssertCalled(t, "List", testutil.IsContext,
+		testutil.IsCoreV1NamespaceListPtr,
+		// verify that the list call did use the correct labelSelector
+		mock.MatchedBy(func(listOptions []client.ListOption) bool {
+			testListOptions := &client.ListOptions{}
+			listOptions[0].ApplyToList(testListOptions)
+			testLabelSelectorString := testListOptions.LabelSelector.String()
+			return len(testLabelSelectorString) > 0 &&
+				testLabelSelectorString == controllers.CommonLabelsAsLabelSelector(addon).String()
+		}))
+	c.AssertCalled(t, "Delete", testutil.IsContext,
+		mock.MatchedBy(func(val *corev1.Namespace) bool {
+			return val.Name == existingNamespace.Name
+		}),
+		mock.Anything)
+}
+
+func TestEnsureDeletionOfUnwantedNamespaces_NamespacesInSpec_matching_NamespacesInCluster(t *testing.T) {
+	c := testutil.NewClient()
+
+	addon := testutil.NewTestAddonWithSingleNamespace()
+	existingNamespace := testutil.NewTestNamespace()
+
+	c.On("List", testutil.IsContext, testutil.IsCoreV1NamespaceListPtr, mock.Anything).
+		Run(func(args mock.Arguments) {
+			arg := args.Get(1).(*corev1.NamespaceList)
+			namespaceList := corev1.NamespaceList{
+				Items: []corev1.Namespace{
+					*existingNamespace,
+				},
+			}
+			namespaceList.DeepCopyInto(arg)
+		}).
+		Return(nil)
+
+	r := &namespaceReconciler{
+		Client: c,
+		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
+	}
+
+	ctx := context.Background()
+	err := r.ensureDeletionOfUnwantedNamespaces(ctx, addon)
+	require.NoError(t, err)
+	c.AssertExpectations(t)
+	c.AssertCalled(t, "List", testutil.IsContext,
+		testutil.IsCoreV1NamespaceListPtr,
+		// verify that the list call did use the correct labelSelector
+		mock.MatchedBy(func(listOptions []client.ListOption) bool {
+			testListOptions := &client.ListOptions{}
+			listOptions[0].ApplyToList(testListOptions)
+			testLabelSelectorString := testListOptions.LabelSelector.String()
+			return len(testLabelSelectorString) > 0 &&
+				testLabelSelectorString == controllers.CommonLabelsAsLabelSelector(addon).String()
+		}))
+}
+
+func TestEnsureDeletionOfUnwantedNamespaces_NoNamespacesInSpec_WithClientError(t *testing.T) {
+	timeoutErr := k8sApiErrors.NewTimeoutError("for testing", 1)
+
+	c := testutil.NewClient()
+	c.On("List", testutil.IsContext, testutil.IsCoreV1NamespaceListPtr, mock.Anything).
+		Return(timeoutErr)
+
+	r := &namespaceReconciler{
+		Client: c,
+		Scheme: testutil.NewTestSchemeWithAddonsv1alpha1(),
+	}
+
+	ctx := context.Background()
+	err := r.ensureDeletionOfUnwantedNamespaces(ctx, testutil.NewTestAddonWithoutNamespace())
+	require.EqualError(t, errors.Unwrap(err), timeoutErr.Error())
+	c.AssertExpectations(t)
 }
